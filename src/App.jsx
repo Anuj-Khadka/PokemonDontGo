802f9bc638374186bc9708198ffd9f221920f4d0
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-28 22:42:43 +0545

    fetch pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react";
 import PokemonList from "./PokemonList";
+import axios from "axios";
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bubblesor", "chalmendar"])
-  
-  return (<>
-  <PokemonList pokemon={pokemon}/>
-  </>);
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/")
+      .then((response) => {
+        setPokemon(response.data.results.map((pokemon) => pokemon.name));
+      })
+      .catch((error) => error.message);
+  }, []);
+
+  return (
+    <>
+      <PokemonList pokemon={pokemon} />
+    </>
+  );
 }
 
 export default App;
