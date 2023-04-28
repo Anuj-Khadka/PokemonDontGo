ea23849e980040855be4a090cecea10e0dd41a08
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-28 23:35:17 +0545

    fetch pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import "./styles.css";
+import axios from "axios";
 import PokemonList from "./PokemonList";
 import Pagination from "./Pagination";
 
