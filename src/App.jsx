f363153c1730da1157e1011c3743579db06c4b1a
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-28 23:29:32 +0545

    fetch pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,56 @@
 import React, { useEffect, useState } from "react";
-import PokemonList from "./PokemonList";
 import axios from "axios";
+import "./styles.css";
+import PokemonList from "./PokemonList";
+import Pagination from "./Pagination";
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon/"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon/"
+  );
+  const [previousPageUrl, setPreviousPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon/"
+  );
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
+    let cancel;
     axios
-      .get("https://pokeapi.co/api/v2/pokemon/")
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
       .then((response) => {
-        setPokemon(response.data.results.map((pokemon) => pokemon.name));
+        setLoading(false);
+        setPokemon(
+          response.data.results.map((pokemon) => pokemon.name)
+        );
+        setNextPageUrl(response.data.next);
+        setPreviousPageUrl(response.data.previous);
       })
       .catch((error) => error.message);
-  }, []);
+
+    return () => {
+      cancel();
+    };
+  }, [currentPageUrl]);
+
+  const goToPrevPage = () => setCurrentPageUrl(previousPageUrl);
+  const goToNextPage = () => setCurrentPageUrl(nextPageUrl);
+
+  if (loading) return "Loading....";
 
   return (
     <>
       <PokemonList pokemon={pokemon} />
+      <Pagination
+        goToNextPage={nextPageUrl ? goToNextPage : null}
+        goToPrevPage={previousPageUrl ? goToPrevPage : null}
+      />
     </>
   );
 }
