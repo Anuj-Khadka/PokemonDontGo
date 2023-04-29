3bb4ca17d1af7caf8e7337cbbbbd2ad08467eae3
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-29 22:28:06 +0545

    update UI

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const PokemonList = ({pokemon }) => {
   return (
     <div className="pokemon-list">
-      {pokemon.map((pokemon) => (
-        <div key={pokemon}>{pokemon}</div>
-      ))}
+      {pokemon.map((pokemon) => (div key={pokemon}>{pokemon}</div>))}
     </div>
   );
 };
