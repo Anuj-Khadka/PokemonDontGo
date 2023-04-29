5c1a619c76b978e01ade4c2382fdc684daf652e1
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-29 22:28:01 +0545

    update UI

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -4,6 +4,7 @@ const Pagination = ({ goToNextPage, goToPrevPage }) => {
   return (
     <div>
       {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
+      
       {goToNextPage && <button onClick={goToNextPage}>Next</button>}
     </div>
   );
