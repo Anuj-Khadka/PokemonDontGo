43df3695fc8064db0b871e38d08eaad7c72cd613
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-04 21:57:50 -0700

    refactor code

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -1,10 +1,7 @@
-import React from "react";
-
 const Pagination = ({ goToNextPage, goToPrevPage }) => {
   return (
     <div>
       {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
-      
       {goToNextPage && <button onClick={goToNextPage}>Next</button>}
     </div>
   );
